7dfb6090d72664767cc42fa89088c1962cbc216f
João Alves <joao.alves@liberali.local>
2021-03-31 07:35:55 -0400

    CONJUNTO HASHCODE AND EQUALS

---
+++ Entites/Client.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashCodeEquals.Entites
+{
+    class Client
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if(!(obj is Client)) {
+                return false;
+            }
+            Client other = obj as Client;
+            return Email.Equals(other.Email);
+        }
+
+        public override int GetHashCode()
+        {
+            return Email.GetHashCode();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using HashCodeEquals.Entites;
+namespace HashCodeEquals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client { Name = "Maria", Email = "maria@gmail.com" };
+            Client b = new Client { Name = "Alex", Email = "alex@gmail.com" };
+
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a == b);
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+        }
+    }
+}
